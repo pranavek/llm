b80b49666ea0429af1dab522c6d60ec4022f1c43
pranavek <pranavek.star@gmail.com>
2025-05-02 15:37:31 -0500

    Add search support

--- mcp/mcp_server/assistant/client.py
+++ mcp/mcp_server/assistant/client.py
@@ -4,7 +4,7 @@ from pydantic_ai.models.openai import OpenAIModel
 from pydantic_ai.providers.openai import OpenAIProvider
 from pydantic_ai.mcp import MCPServerStdio
 
-server = MCPServerStdio(  
+assistant_server = MCPServerStdio(  
     'go',
     args=[
         'run',
@@ -12,13 +12,23 @@ server = MCPServerStdio(
     ]
 )
 
-
+duckduckgo_server = MCPServerStdio(
+    'uvx',
+    args=["duckduckgo-mcp-server"]
+)
 
 ollama_model = OpenAIModel(
-    model_name='mistral-small3.1',  
+    model_name='qwen2.5',  
     provider=OpenAIProvider(base_url='http://localhost:11434/v1'), 
 )
-agent = Agent(model=ollama_model, mcp_servers=[server])
+agent = Agent(
+    model=ollama_model, 
+    mcp_servers=[assistant_server, duckduckgo_server],
+    instuctions="""
+        You are a helpful assistant. You can use the DuckDuckGo search engine to find information.
+        You can also run a Go program to perform tasks.
+        """
+)
 
 
 async def main():
